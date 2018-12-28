32e72904749e106c36743e83aadaea1964101177
Kevin Bieri <bierike@gmail.com>
2018-12-28 12:14:39 +0100

    Fix linting errors on generated project

--- {{cookiecutter.project_name}}/tests/unit/{{cookiecutter.library_name}}.spec.js
+++ {{cookiecutter.project_name}}/tests/unit/{{cookiecutter.library_name}}.spec.js
@@ -1,6 +1,5 @@
 import {{cookiecutter.component_name}} from '@/components/{{cookiecutter.component_name}}.vue';
 import { localMount } from './support';
-import Vue from 'vue';
 
 describe('{{cookiecutter.component_name}}', () => {
   test('Component renders', async () => {
